41f62749ea1d220a80b72ac0ce3d70fe70ee656a
Lunall <wooju407@naver.com>
2020-10-09 21:22:29 +0900

    재귀로 하향식 분석, 상향식 분석 연습

--- JavaData/src/Quiz/RecurQui1.java
+++ JavaData/src/Quiz/RecurQui1.java
@@ -1,4 +1,21 @@
 package Quiz;
 
+import java.util.Scanner;
+
 public class RecurQui1 {
+
+    static void recur(int n){
+        if(n>0){
+            //System.out.println("recur(n-1)="+ (n-1) );
+            recur(n-1 );
+            System.out.println("n="+n);
+            recur(n-2);
+        }
+    }
+
+    public static void main(String[] args) {
+        //recur(4);
+
+        recur(3);
+    }
 }
