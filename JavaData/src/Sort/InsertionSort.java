122908033350a844b0748889ca7feefa779c2ff1
Lunall <wooju407@naver.com>
2020-10-19 20:36:40 +0900

    수정

--- JavaData/src/Sort/InsertionSort.java
+++ JavaData/src/Sort/InsertionSort.java
@@ -4,13 +4,16 @@ public class InsertionSort {
 
     static void insertionSort(int [] arr, int n){
         for(int i= 1 ; i<n ; i++){
+            System.out.println("회차 "+i+"회" );
             int j;
             int temp=arr[i];
 
             for(j = i; j>0 && arr[j-1] > temp; j--){
+                System.out.println("arr[j-1]= "+arr[j-1] + " temp"+temp);
                 arr[j]= arr[j-1];
             }
-            System.out.println("j1= "+j +" temp"+temp);
+            System.out.printf("%d번째 배열에", j);
+            System.out.printf(" %d값을 넣습니다 \n",temp);
             arr[j]=temp;
         }
     }
