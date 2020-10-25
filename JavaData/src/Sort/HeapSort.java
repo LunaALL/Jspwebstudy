42ca8659632b7b3a7118a5860c4286cf19a833e3
Lunall <wooju407@naver.com>
2020-10-25 21:18:52 +0900

    힙정렬 최종 구현. 힙의 특징은 루트가 제일 크며 부모보다 자식이 클수없음. 왼쪽 자식보다 오른쪽 자식이 더 크게

--- JavaData/src/Sort/HeapSort.java
+++ JavaData/src/Sort/HeapSort.java
@@ -22,8 +22,31 @@ public class HeapSort {
             arr[parent] = arr[child];
         }
         arr[parent]= temp;
-
     }
+
+    static void heapSort(int[] arr, int n){
+        for (int i= (n-1)/2 ; i>=0 ; i--){  //arr[i]~arr[i-1]를 힙으로 만들기.
+            toHeap(arr,i,n-1);
+        }
+
+        for(int i=n-1; i>0 ; i--){
+            swap(arr , 0 , i);
+            toHeap(arr,0,i-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int [] arr= {8,3,5,1,7,4,10,9,13,22};
+
+        heapSort(arr, arr.length);
+
+        for(int i=0; i<arr.length ; i++){
+            System.out.println("힙정렬 완료!! ");
+            System.out.println("arr["+i+"]="+arr[i]);
+        }
+    }
+
+
 
 
 }
