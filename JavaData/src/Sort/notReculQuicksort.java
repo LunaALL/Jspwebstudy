0d63c383084f513ecd8231c856717e66f5ebe88c
Lunall <wooju407@naver.com>
2020-10-27 20:39:34 +0900

    비 재귀적인 퀵 정렬. 스택을 이용하면 된다.

--- JavaData/src/Sort/notReculQuicksort.java
+++ JavaData/src/Sort/notReculQuicksort.java
@@ -4,31 +4,41 @@ import java.util.Stack;
 
 public class notReculQuicksort {
 
-    public static void swap(int[] arr, int a, int b){
-        int temp=arr[a];
-        arr[a]=arr[b];
-        arr[b]=temp;
+    public static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 
 
-    static void QuickSort(int []arr, int left, int right){
-        Stack<Integer> lst =new Stack<Integer>();
-        Stack<Integer> rst =new Stack<Integer>();
+    static void QuickSort(int[] arr, int left, int right) {
+        Stack<Integer> lst = new Stack<Integer>();
+        Stack<Integer> rst = new Stack<Integer>();
 
         lst.push(left);
         rst.push(right);
 
-        while(lst.isEmpty() != true){
-            int pleft = left =lst.pop();
-            int pright= right = rst.pop();
-            int x = arr[(left+right) /2 ];
+        while (lst.isEmpty() != true) {
+            int pleft = left = lst.pop();
+            int pright = right = rst.pop();
+            int x = arr[(left + right) / 2];
 
-            do{
-                while( arr[pleft] < x) pleft++;
-                while (arr[pr] > x) pr--;
-                if(pleft<=pr)
-                    swap(arr,pleft++, pr--);
-            }while(pleft<=pr);
+            do {
+                while (arr[pleft] < x) pleft++;
+                while (arr[pright] > x) pright--;
+                if (pleft <= pright)
+                    swap(arr, pleft++, pright--);
+            } while (pleft <= pright);
+
+            if (left < pright) {
+                lst.push(left);
+                rst.push(pright);
+            }
+
+            if (pleft < right) {
+                lst.push(pleft);
+                rst.push(right);
+            }
 
 
         }
@@ -37,6 +47,16 @@ public class notReculQuicksort {
     }
 
     public static void main(String[] args) {
+
+        int[] arr ={6,5,9,4,2,10,16,22,1};
+
+        QuickSort(arr,0,arr.length-1);
+
+        for (int x: arr
+             ) {
+            System.out.print(x+",");
+
+        }
 
     }
 }
