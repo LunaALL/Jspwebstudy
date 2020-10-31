5a751b81213b5de01c9701fc1741bbfbf24c06dc
Lunall <wooju407@naver.com>
2020-10-31 20:31:00 +0900

    메인 클래스 기능 점검

--- JavaData/src/Main2root.java
+++ JavaData/src/Main2root.java
@@ -16,8 +16,14 @@ public class Main2root {
         s2.add(12);
         s2.remove(25);
 
-        System.out.println(s2.toString());
         System.out.println(s1.toString());
+        System.out.println(s2.toString());
+        System.out.println(s2.indexOf(25));
 
+        System.out.println("집합 s1에 15는" + (s1.contains(15) ? "포함됩니다." : "포함되지 않습니다." ) );
+
+        System.out.println(s1.equalTo(s2) ? "서로 같습니다": "서로 다른 집합입니다.");
+        s2.copyTo(s3);
+        System.out.println(s2.equalTo(s3)? "서로 같습니다." : "서로 다른 집합입니다.");
     }
 }
