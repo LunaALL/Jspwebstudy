8b3872264066a9372554cb63b5c8a2b51a72bd00
Lunall <wooju407@naver.com>
2020-11-04 21:53:15 +0900

    자바 기본 스트링 라이브러리 안에있는 indexof 써보기

---
+++ JavaData/src/Search/StringIndexofuse.java
@@ -0,0 +1,38 @@
+package Search;
+
+import java.util.Scanner;
+
+public class StringIndexofuse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("텍스트 :");
+        String Plaintxt= sc.nextLine();
+
+        System.out.println("찾을 텍스트:");
+        String match = sc.nextLine();
+
+        int idx1= Plaintxt.indexOf(match);
+        int idx2= Plaintxt.indexOf(match);
+
+        if(idx1 == -1)
+            System.out.println("텍스트 안에 패턴이 없습니다.");
+        else{
+            int len1=0;
+            for(int i=0; i< idx1; i++){
+                len1 += Plaintxt.substring(i,i+1).getBytes().length;
+            }
+            len1+=match.length();
+            int len2=0;
+            for(int i=0; i<idx2; i++){
+                len2 += Plaintxt.substring(i, i+1).getBytes().length;
+            }
+            len2 += match.length();
+
+
+            
+
+
+        }
+    }
+}
