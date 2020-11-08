51df5a76f25b18d923f1d77a9bfc2197f22242f7
Lunall <wooju407@naver.com>
2020-11-08 22:41:45 +0900

    프린트문 수정

--- JavaData/src/Search/bmMatch.java
+++ JavaData/src/Search/bmMatch.java
@@ -6,7 +6,7 @@ public class bmMatch {
         int pp; //pat커서
         int txtLen = txt.length(); //txt의 문자 갯수
         int patLen = pat.length(); //pat의 문자 개수
-        int [] skip= new int[Character.MAX_VALUE+1];   //건너뛰기표
+        int [] skip= new int[Character.MAX_VALUE+1];   //건너뛰기표, char 최댓값을 기준으로 배열 생성.
 
         for(pt= 0; pt <= Character.MAX_VALUE; pt++){
             skip[pt]=patLen;
@@ -14,6 +14,18 @@ public class bmMatch {
         for (pt=0; pt< patLen -1 ; pt++){
             skip[pat.charAt(pt)] = patLen - pt- 1;
         }
+        System.out.println("건너띄기표 출력");
+        for (int x: skip
+             ) {
+            if (x==pat.length()){
+                //찾을 패턴에 존재할 수 있는 모든 문자의 옮길 크기를 계산하고 저장하기 때문에 중복이 많아 필요없는 부분은 제외함.
+                continue;
+            }
+            System.out.print(x+",");
+
+        }
+
+        System.out.println("");
         while(pt<txtLen){
             pp=patLen-1;
 
@@ -33,7 +45,9 @@ public class bmMatch {
         String txt= "ABFKEJGKDEFLDKAVLDS";
         String match= "EFLD";
 
-        System.out.println(bmMatch(txt,match));
+        int idx=bmMatch(txt,match);
+        System.out.println(txt +"  |||| match = "+match);
+        System.out.println( idx<0 ? "해당하는 패턴을 찾을 수 없습니다." : "원본 텍스트"+idx+"번째에서 찾았습니다");
     }
 
 }
