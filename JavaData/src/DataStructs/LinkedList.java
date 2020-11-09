669ed4efc1c367cb54620627334df7ab8042c8ce
Lunall <wooju407@naver.com>
2020-11-09 23:02:14 +0900

    서치 메서도

--- JavaData/src/DataStructs/LinkedList.java
+++ JavaData/src/DataStructs/LinkedList.java
@@ -1,5 +1,7 @@
 package DataStructs;
 
+import java.util.Comparator;
+
 public class LinkedList <E> {
 
     class Node<E> {
@@ -17,5 +19,20 @@ public class LinkedList <E> {
 
     private Node<E> head;
     private Node<E> crnt;
+
+    // 노드 검색
+    public E search(E obj, Comparator<? super E> c) {
+        Node<E> ptr = head;							// 현재 스캔중인  노드
+
+        while (ptr != null) {
+            if (c.compare(obj, ptr.data) == 0) {	// 검색 성공
+                crnt = ptr;
+                return ptr.data;
+            }
+            ptr = ptr.next;							// 다음 노드를 선택
+        }
+        return null;								// 검색 실패
+    }
+
 
 }
