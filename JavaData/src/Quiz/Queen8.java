d47e548cbb77859ebd6a5b739dcbc6c900bf04d6
Lunall <wooju407@naver.com>
2020-11-12 22:41:14 +0900

    8퀸 문제 풀기

---
+++ JavaData/src/Quiz/Queen8.java
@@ -0,0 +1,31 @@
+package Quiz;
+
+public class Queen8 {
+    static int[] pos = new int[8]; //각 열의 퀸 위치
+
+    static void print() {
+        for (int i=0; i<8;i++){
+            System.out.printf("%2d",pos[i]);
+        }
+        System.out.println(" ");
+    }
+
+
+    static void set(int i){
+        for (int j=0; j<8 ; j++){
+            pos[i]=j;
+            if (i==7)
+                print();
+            else
+                set(i+1);
+        }
+    }
+
+    public static void main(String[] args) {
+        set(0);
+    }
+    
+
+
+
+}
