ae978f3c08f486b1881daeef072325784aaeecd8
Lunall <wooju407@naver.com>
2020-11-13 22:50:33 +0900

    8퀸 문제에 해당하는 답안 출력.

--- JavaData/src/Quiz/Queen8.java
+++ JavaData/src/Quiz/Queen8.java
@@ -1,7 +1,10 @@
 package Quiz;
 //각 열에 1개의 퀸을 배치하는 조합을 재귀적으로 나열.
 public class Queen8 {
-    static boolean[] flag= new boolean[8];
+    //각 행에 퀸을 배치했는지 체크
+    static boolean[] flag_a= new boolean[8];
+    static boolean[] flag_b= new boolean[15];
+    static boolean[] flag_c= new boolean[15];  //대각선 방향으로 퀸을 배치했는지 체크하는 flagb,c
     static int[] pos = new int[8]; //각 열의 퀸 위치
 
     //각 열의 퀸의 위치를 출력.
@@ -16,14 +19,14 @@ public class Queen8 {
     //i열에 퀸을 놓습니다.
     static void set(int i){
       for (int j=0; j<8;j++ ){
-          if(flag[j] == false) {  //j행에 퀸을 배치하지 않았다면
+          if(flag_a[j] == false && flag_b[i+j] == false && flag_c[i-j+7]== false) {  //j행에 퀸을 배치하지 않았다면
               pos[i] = j; // 퀸을 j행에 배치.
               if (i == 7) //모든 열에 배치한 경우.
                   print();
               else {
-                  flag[j] = true;
+                  flag_a[j] = flag_b[i+j] = flag_c[i-j+7]=  true;
                   set(i + 1);
-                  flag[j] = false;
+                  flag_a[j] = flag_b[i+j] = flag_c[i-j+7]= false;
               }
           }
       }
