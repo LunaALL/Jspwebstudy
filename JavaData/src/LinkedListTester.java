6c3e4e1f445a58eaf71b980f73233b3663bdbfde
Lunall <wooju407@naver.com>
2020-11-16 21:57:27 +0900

    정렬메서드, Comparator 이용. Data 클래스

--- JavaData/src/LinkedListTester.java
+++ JavaData/src/LinkedListTester.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class LinkedListTester {
@@ -16,8 +18,30 @@ public class LinkedListTester {
             //회원 번호 , 이름 반환
             return "(" + no + ")" + name;
         }
+        //데이터 입력용
+        void scanData(String guide, int sw){
+            System.out.println(guide + "할 데이터를 입력하셈.");
 
+            if ((sw & N0)==N0 ){
+                System.out.print("번호 :");
+                no = std.nextInt();
+            }
+            if ((sw & NAME) == NAME) {
+                System.out.print("이름");
+                name = std.nextLine();
+            }
+        }
 
+        //데이터 정렬. 회원번호 기준.
+        public static final Comparator<Data> NO_ORDER = new NoOrderComparator();
+
+        private static class NoOrderComparator implements Comparator <Data> {
+
+            @Override
+            public int compare(Data o1, Data o2) {
+                return (o1.no > o2.no) ? 1: (o1.no < o2.no) ? -1:0;
+            }
+        }
     }
 
 
