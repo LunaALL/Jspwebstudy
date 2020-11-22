4ffe7cf463c675c4fd2fb783b66ebf01714dbc46
Lunall <wooju407@naver.com>
2020-11-22 18:34:38 +0900

    이진트리

--- JavaData/src/DataStructs/BinaryTree.java
+++ JavaData/src/DataStructs/BinaryTree.java
@@ -1,4 +1,46 @@
 package DataStructs;
 
-public class BinaryTree {
+import java.util.Comparator;
+
+//일반화 클래스. 타입 여러가지를 지정 가능.
+public class BinaryTree<k,v> {
+    static class Node<K,V>{
+        private K key;
+        private V data;
+        private Node<K,V> left;
+        private Node<K,V>  right;
+
+        public Node(K key, V data, Node<K, V> left, Node<K, V> right) {
+            this.key = key;
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getData() {
+            return data;
+        }
+
+        void DataPrint(){
+            System.out.println(data);
+        }
+    }
+
+    private Node<K,V> root;
+    private Comparator<? super K> comparator =null;
+
+    public BinaryTree(){
+        root= null;
+    }
+
+    public BinaryTree(Comparator<? super K> c) {
+        this();
+        comparator=c;
+
+
+    }
 }
