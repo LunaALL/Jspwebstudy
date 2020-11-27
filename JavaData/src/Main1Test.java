6efff17b45705344206b0d71b451e9c75a78dcbc
Lunall <wooju407@naver.com>
2020-11-27 21:40:48 +0900

    Comparable 인터페이스 구현

--- JavaData/src/Main1Test.java
+++ JavaData/src/Main1Test.java
@@ -1,7 +1,14 @@
-import DataStructs.LinkedList;
+import basicStu.Student;
+
+import java.math.BigInteger;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main1Test {
     public static void main(String[] args) {
+        /*
         LinkedList<Integer> L1= new LinkedList<>();
         L1.addFirst(10);
         L1.addFirst(20);
@@ -14,6 +21,31 @@ public class Main1Test {
         StringBuilder sb =new StringBuilder(a1);
          sb.reverse();
         System.out.println(sb);
+
+         */
+
+        Student st1 = new Student(1, "jdk", "솔루션1","연구원",new BigInteger("2800"));
+        Student st2 = new Student(2, "bab", "솔루션1","연구원",new BigInteger("3200"));
+        Student st3 = new Student(3, "amateur", "솔루션1","연구원",new BigInteger("5500"));
+        Student st4 = new Student(4, "jeong", "솔루션1","연구원",new BigInteger("6500"));
+        Student st5 = new Student(5, "pro", "솔루션1","연구원",new BigInteger("7000"));
+
+        List<Student> employ= new LinkedList<>();
+        employ.add(st2);
+        employ.add(st3);
+        employ.add(st1);
+        employ.add(st4);
+        employ.add(st5);
+        System.out.println(employ.get(0));
+        System.out.println(employ.get(1));
+        System.out.println(employ.get(2));
+        System.out.println(employ.get(3));
+        System.out.println(employ.get(4));
+        Collections.sort(employ);
+        System.out.println("####");
+        System.out.println(employ);
+
+
 
     }
 }
--- JavaData/src/basicStu/Student.java
+++ JavaData/src/basicStu/Student.java
@@ -11,7 +11,27 @@ public class Student implements Comparable<Student> {
 
     @Override
     public int compareTo(Student o) {
-        //정렬에 있어서 앞에 오고 싶을 때 음수를 내보내면 됨. 
-        return 0;
+        //정렬에 있어서 앞에 오고 싶을 때 음수를 내보내면 됨.
+        return this.name.compareTo(o.name);
+        //return this.id - o.id; 만일 사번이 기준이라면 ??
+    }
+
+    public Student(int id, String name, String department, String position, BigInteger sales) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.position = position;
+        this.sales = sales;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", department='" + department + '\'' +
+                ", position='" + position + '\'' +
+                ", sales=" + sales +
+                '}';
     }
 }
