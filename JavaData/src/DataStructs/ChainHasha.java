b6cc819f5b47a602f5a9591e5f1ce9430e9798e0
Lunall <wooju407@naver.com>
2020-12-14 20:27:10 +0900

    메서드 몇개 추가

--- JavaData/src/DataStructs/ChainHasha.java
+++ JavaData/src/DataStructs/ChainHasha.java
@@ -20,7 +20,59 @@ public class ChainHasha <K,V> {
         public V getValue() {
             return data;
         }
+
+        public int hashCode(){
+            return key.hashCode();
+        }
+
     }
+
+    private int	size;						// 해시 테이블의 크기
+    private Node<K,V>[] table;				// 기본 해시 테이블,
+
+    // 생성자
+    public ChainHasha(int capacity) {
+        try {
+            table = new Node[capacity];
+            this.size = capacity;
+        } catch (OutOfMemoryError e) {		// 테이블을 생성할 수 없음
+            this.size = 0;
+        }
+    }
+
+    // 해시값을 구함
+    public int hashValue(Object key) {
+        return key.hashCode() % size;
+    }
+
+    private V search(K key){
+        int hash = hashValue(key);
+        Node<K,V> p = table[hash];
+
+        while (p != null){
+            if (p.getKey().equals(key))
+                return p.getValue();
+            p=p.next;
+        }
+        return null;
+    }
+
+    public int add(K key, V data){
+       int hash = hashValue(key);
+       Node<K,V> p = table[hash];
+
+       while (p != null){
+           if (p.getKey().equals(key))
+               return 1;
+           p=p.next;
+       }
+       Node<K,V> temp = new Node<K,V>(key,data,table[hash]);
+       table[hash]=temp;
+       return 0;
+    }
+
+
+
 
 
 }
