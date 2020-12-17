e1164f991ef455461ea1194967568b407fe9cd16
Lunall <wooju407@naver.com>
2020-12-17 20:05:25 +0900

    오류 해결..

--- JavaData/src/DataStructsmain/ChainHashTestermain.java
+++ JavaData/src/DataStructsmain/ChainHashTestermain.java
@@ -7,33 +7,34 @@ import java.util.Scanner;
 public class ChainHashTestermain {
     static Scanner stdIn = new Scanner(System.in);
 
+    // 데이터 (회원번호 + 이름)
     static class Data {
-        static final int NO = 1;
-        static final int NAME = 2;
+        static final int NO = 1;        // 번호를 읽어 들일까요?
+        static final int NAME = 2;        // 이름을 읽어 들일까요?
 
-        private Integer no;
-        private String name;
+        private Integer no;                // 회원번호 (키 값)
+        private String name;            // 이름
 
+        // 키 값
         Integer keyCode() {
-            return NO;
+            return no;
         }
 
-        @Override
+        // 문자열 표현을 반환합니다.
         public String toString() {
-            return "ChainHashTestermain{" +
-                    "name='" + name + '\'' +
-                    '}';
+            return name;
         }
 
+        // 데이터를 입력합니다.
         void scanData(String guide, int sw) {
-            System.out.println(guide + "할 데이터를 입력하세요");
+            System.out.println(guide + "할 데이터를 입력하세요.");
 
             if ((sw & NO) == NO) {
-                System.out.print("번호 : ");
+                System.out.print("번호：");
                 no = stdIn.nextInt();
             }
             if ((sw & NAME) == NAME) {
-                System.out.print("이름 : ");
+                System.out.print("이름：");
                 name = stdIn.next();
             }
         }
@@ -65,38 +66,40 @@ public class ChainHashTestermain {
         }
     }
 
-    static Menu SelectMenu(){
+    // 메뉴 선택
+    static Menu SelectMenu() {
         int key;
         do {
-            for(Menu m : Menu.values())
-                System.out.printf("(%d) %s  ", m.ordinal(), m.getMessage() );
-            System.out.print(" : ");
-            key=stdIn.nextInt();
-        } while (key < Menu.ADD.ordinal() || key> Menu.TERMINATE.ordinal() );
+            for (Menu m : Menu.values())
+                System.out.printf("(%d) %s  ", m.ordinal(), m.getMessage());
+            System.out.print("：");
+            key = stdIn.nextInt();
+        } while (key < Menu.ADD.ordinal() || key > Menu.TERMINATE.ordinal());
+
         return Menu.MenuAt(key);
     }
 
     public static void main(String[] args) {
-        Menu menu;								// 메뉴
-        Data data;								// 추가용 데이터 참조
-        Data temp = new Data();					// 입력용 데이터
+        Menu menu;                                // 메뉴
+        Data data;                                // 추가용 데이터 참조
+        Data temp = new Data();                    // 입력용 데이터
 
         ChainHasha<Integer, Data> hash = new ChainHasha<Integer, Data>(13);
 
         do {
             switch (menu = SelectMenu()) {
-                case ADD :							// 추가
+                case ADD:                            // 추가
                     data = new Data();
                     data.scanData("추가", Data.NO | Data.NAME);
                     hash.add(data.keyCode(), data);
                     break;
 
-                case REMOVE :						// 삭제
+                case REMOVE:                        // 삭제
                     temp.scanData("삭제", Data.NO);
                     hash.remove(temp.keyCode());
                     break;
 
-                case SEARCH :						// 검색
+                case SEARCH:                        // 검색
                     temp.scanData("검색", Data.NO);
                     Data t = hash.search(temp.keyCode());
                     if (t != null)
@@ -105,7 +108,7 @@ public class ChainHashTestermain {
                         System.out.println("그 데이터가 없습니다.");
                     break;
 
-                case DUMP : 						// 출력
+                case DUMP:                        // 출력
                     hash.dump();
                     break;
             }
